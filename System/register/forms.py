e3431354e476479d484fd6d5f67d3366ab9afb2b
ardel <ardeltiocojefflauron@gmail.com>
2024-10-03 00:51:03 +0800

    Updated Registration

--- System/register/forms.py
+++ System/register/forms.py
@@ -2,14 +2,19 @@ from django import forms
 from .models import User 
 
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control',
-                                                                 'placeholder':'Password'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    
+    # Define the choices outside the Meta class
+    is_worker_choice = [
+        ('False', 'Yes'),
+        ('True', 'No')
+    ]
+    
+    is_worker = forms.ChoiceField(choices=is_worker_choice, widget=forms.RadioSelect)
+
     class Meta:
         model = User
-        fields = ['username','password']
+        fields = ['username', 'password', 'is_worker']
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control',
-                                               'placeholder': 'Username'})
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
         }
-
-
