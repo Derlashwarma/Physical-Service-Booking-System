0e873b7d96d59760a9baff61c51808ce04b245c4
ardel <ardeltiocojefflauron@gmail.com>
2024-10-16 17:36:24 +0800

    (LAURON) log in complete

--- System/login/views.py
+++ System/login/views.py
@@ -16,13 +16,13 @@ def login_user(request):
                 if user is not None and check_password(password, user.password):
                     request.session['username'] = user.username
                     request.session['id'] = user.id
+                    login(request,user)
                     return HttpResponse("Logged in")
                 else:
-                    error = "invalid credintials"
-                    form = LoginForm()
-                    return render(request, 'login.html',{'error': error, 'form':form} )
+                    form.add_error('password',"Incorrect Password")
+                    return render(request, 'login.html',{'form':form} )
             except Exception:
-                form.add_error(None, 'Something happend bad')    
+                form.add_error('username', 'Username not found')    
     else:
         form = LoginForm()
     return render(request,'login.html', {'form':form})
