101e66450c0a49011a95e9e617eb1b2c7b0cb6b8
Mars0827 <marsbenitez1@gmail.com>
2024-11-03 08:15:12 +0800

    employer can create job

--- System/employer/views.py
+++ System/employer/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from job.models import Job
 
 # Create your views here.
 def employer_feed(request):
-    return render(request, 'employer_feed.html')
+    jobs = Job.objects.filter(is_done=False)  
+    context = {
+        'jobs': jobs,
+    }
+    return render(request, 'employer_feed.html', context)
