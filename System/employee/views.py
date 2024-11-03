4adb53cf1e94feea5b376fec1cf70c5b996d93bd
Mars0827 <marsbenitez1@gmail.com>
2024-11-03 08:14:44 +0800

    Employee can see jobs

--- System/employee/views.py
+++ System/employee/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from job.models import Job
 # Create your views here.
 def employee_feed(request):
-    return render(request, 'employee_feed.html')
+    jobs = Job.objects.filter(is_done=False)
+    context = {
+        'jobs': jobs,
+    }
+    return render(request, 'employee_feed.html', context)
