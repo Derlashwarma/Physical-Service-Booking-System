c8eecf0c8ebc21625365ac9f0834cfb8ba58a484
ardel <ardeltiocojefflauron@gmail.com>
2024-11-08 18:42:58 +0800

    (Lauron) Created Models

--- System/chat/models.py
+++ System/chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from register.models import CustomUser
 
-# Create your models here.
+class Conversation(models.Model):
+    conversation_name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.conversation_name
+    
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, related_name='chat_messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    message = models.CharField(max_length=500, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username} : {self.message}'
