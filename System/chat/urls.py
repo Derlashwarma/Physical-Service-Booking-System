0d182736c8624c3a14e6ef4b68b8aeb7089249d8
ardel <ardeltiocojefflauron@gmail.com>
2024-11-09 09:40:33 +0800

    Chat Implementation is Fully Working

--- System/chat/urls.py
+++ System/chat/urls.py
@@ -6,5 +6,4 @@ app_name="chat"
 urlpatterns = [
     path('', chat_index, name='chat_index'),
     path('<str:username>', chat_conversation, name='conversation'),
-    path('conversation/<str:username>/send/', chat_conversation, name='send_message'),
 ]
