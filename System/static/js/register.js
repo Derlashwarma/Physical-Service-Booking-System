5d4d5165285e380f78050c8162e7fd5d84b88845
ardel <ardeltiocojefflauron@gmail.com>
2024-12-03 15:31:21 +0800

    (Lauron) Improves UX

--- System/static/js/register.js
+++ System/static/js/register.js
@@ -10,4 +10,14 @@ $(document).ready(()=>{
         $("#client").removeClass('border-green-700')
     })
     
+    $('#show_password').click(function() {
+        var passwordField = $('#id_password');
+        var fieldType = passwordField.attr('type');
+
+        if (fieldType === 'password') {
+            passwordField.attr('type', 'text');
+        } else {
+            passwordField.attr('type', 'password');
+        }
+    });
 })
